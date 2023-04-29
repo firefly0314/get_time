9c460d47845419930fc16f14b8487ccb02f040a3
taka-mein <takamasa3140@gmail.com>
2023-04-30 00:20:06 +0900

    0.3

--- get_time.py
+++ get_time.py
@@ -20,7 +20,7 @@ def time_choice():
                     time_bool = False
                     time.sleep(1)
 
-def bcd(txt):
+def bcd(txt):#bcdコードに変換
     txt_temp = str(txt)
     if 99 < txt < 1000 : #3桁の場合
         Hundreds_digit  = int(txt_temp[0])
@@ -32,7 +32,7 @@ def bcd(txt):
     elif 0 < txt < 9 :  #１桁の場合
         nes_digit      = int(txt_temp[0])
     else:
-        print(datetime.datetime.now(),":value error")
+        return 
 
 #変数とオプション
 options = Options()
@@ -41,8 +41,8 @@ options.add_argument('--headless')#実際にブラウザを表示しないオプ
 options.use_chromium = True
 loop_time = 60
 retry = 3
-
-time_choice()
+#mainloop
+time_choice()#時間合わせ
 time_bool = False
 while True: 
     while not time_bool:
@@ -53,31 +53,21 @@ while True:
             time.sleep(1)#処理待ち
             html = driver.page_source
             driver.close()#ウェブページを閉じる
+            
             soup = BeautifulSoup(html, "html.parser") # BeautifulSoupで扱えるようにパースします
-
             txt = soup.text#テキストデータのみ抽出
             txt = txt[txt.find("お届け時間")+5:]#以下の２行で時間のみに加工
             txt = txt[:txt.find("分"):]
 
-            if txt.isdigit():#整数かどうか判断
-                txt = int(txt)
-                txt = txt-10 #テイクアウト時間算出
-                print (datetime.datetime.now(),txt)
-                #bcdコードに変換
-                txt_temp = str(txt)
+            if txt.isdigit():#整数かどうか判断                
                 retry_bool = True
-                if 99 < txt < 1000 : #3桁の場合
-                    Hundreds_digit  = int(txt_temp[0])
-                    tens_digit      = int(txt_temp[1])
-                    ones_digit      = int(txt_temp[2])
-                elif 9 < txt < 99:  #2桁の場合
-                    tens_digit      = int(txt_temp[0])
-                    ones_digit      = int(txt_temp[1])
-                elif 0 < txt < 9 :  #１桁の場合
-                    ones_digit      = int(txt_temp[0])
+                break
             else:
-                print(datetime.datetime.now(),":value error")
                 retry_bool = False
-            retry_nam = 0
-            break
-    time.sleep(loop_time-2)
+
+    
+        txt = int(txt)
+        txt = txt-10 #テイクアウト時間算出
+        retry_nam = 0
+        print (datetime.datetime.now(),txt)
+        time.sleep(loop_time-2)
