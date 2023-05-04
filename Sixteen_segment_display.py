3a39f4eaca908ff084f4c57188e0f0db207617db
taka-sub1 <takamasa3140@gmail.com>
2023-05-04 22:23:16 +0900

    16seg

---
+++ Sixteen_segment_display.py
@@ -0,0 +1,161 @@
+def Sixteen_segment_display(str_in):
+    if   str_in == "A":
+        seg_deta = [1,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+    
+    elif str_in == "B":
+        seg_deta = [1,1,0,1,1,1,1,1,1,1,0,0,1,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "C":
+        seg_deta = [1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "D":
+        seg_deta = [1,1,1,1,1,1,0,0,0,0,0,1,0,0,1,0,0]
+        return seg_deta
+
+    elif str_in == "E":
+        seg_deta = [1,1,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "F":
+        seg_deta = [1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "G":
+        seg_deta = [1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "H":
+        seg_deta = [0,0,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "I":
+        seg_deta = [1,1,0,0,1,1,0,0,0,0,0,1,0,0,1,0,0]
+        return seg_deta
+
+    elif str_in == "J":
+        seg_deta = [0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "K":
+        seg_deta = [0,0,0,0,0,0,1,1,1,0,0,0,1,0,0,1,0]
+        return seg_deta
+
+    elif str_in == "L":
+        seg_deta = [0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "M":
+        seg_deta = [0,0,1,1,0,0,1,1,0,0,1,0,1,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "N":
+        seg_deta = [0,0,1,1,0,0,1,1,0,0,1,0,0,0,0,1,0]
+        return seg_deta
+
+    elif str_in == "O":
+        seg_deta = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "P":
+        seg_deta = [1,1,1,0,0,0,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "Q":
+        seg_deta = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0]
+        return seg_deta
+
+    elif str_in == "R":
+        seg_deta = [1,1,1,0,0,0,1,1,1,1,0,0,0,0,0,1,0]
+        return seg_deta
+
+    elif str_in == "S":
+        seg_deta = [1,1,0,1,1,1,0,1,1,1,1,0,0,0,0,1,0]
+        return seg_deta
+
+    elif str_in == "T":
+        seg_deta = [1,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0]
+        return seg_deta
+
+    elif str_in == "U":
+        seg_deta = [0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "V":
+        seg_deta = [0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0]
+        return seg_deta
+
+    elif str_in == "W":
+        seg_deta = [0,0,1,1,0,0,1,1,0,0,0,0,0,1,0,1,0]
+        return seg_deta
+
+    elif str_in == "X":
+        seg_deta = [0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0]
+        return seg_deta
+
+    elif str_in == "Y":
+        seg_deta = [0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0]
+        return seg_deta
+
+    elif str_in == "Z":
+        seg_deta = [1,1,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0]
+        return seg_deta
+
+    elif str_in == "0":
+        seg_deta = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "1":
+        seg_deta = [0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0]
+        return seg_deta
+
+    elif str_in == "2":
+        seg_deta = [1,1,1,0,1,1,1,0,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "3":
+        seg_deta = [1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "4":
+        seg_deta = [0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "5":
+        seg_deta = [1,1,0,1,1,1,0,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "6":
+        seg_deta = [1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    elif str_in == "7":
+        seg_deta = [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        return seg_deta
+
+
+    elif str_in == "8":
+        seg_deta = [1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+
+    elif str_in == "9":
+        seg_deta = [1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0]
+        return seg_deta
+
+    else:
+        seg_deta= [1,1,1,1,1,1,1,1,0,0,1,0,1,1,0,1,0]
+
+def str_slice(str_in):
+    str_out=[]
+    for i in range(len(str_in)):
+        str_temp = str_in[i:i+1]
+        #Sixteen_segment_display(str_temp)
+        str_out.append( Sixteen_segment_display(str_temp))
+        return str_out
+
+
+n=str_slice("PIZA")
+print(n)
