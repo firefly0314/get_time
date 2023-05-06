7c5f9036b1c6ff43d17836a70d3dad10bea54d36
taka-mein <takamasa3140@gmail.com>
2023-05-07 00:11:59 +0900

    0.6

--- get_time.py
+++ get_time.py
@@ -1,30 +1,13 @@
 # coding: UTF-8
 import datetime
-import time
 import logging
+import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 import lib.Sixteen_segment_display as SSD
-
-
-def time_adjustment():
-    time_bool = False
-    while not time_bool:
-        time_now = datetime.datetime.now().minute
-        if -1 < time_now < 60:
-            for i in range(0,60,10):
-                time_division_remainder = (time_now+10)%(i+10)
-                if time_division_remainder == 0:
-                    time_bool = True
-                    break
-                else:
-                    time_bool = False
-                    time.sleep(1)
-
-
 #変数とオプション
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -39,24 +22,24 @@ while True:
     while retry_bool:
         for retry_nam in range(1, retry+1):
             time_adjustment()#時間合わせ
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":get web source start")
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":get web source start")
             driver = webdriver.Chrome(options=options) #ブラウザを起動する
             time.sleep(1)#起動時間待ち
             driver.get("https://napolipizzademae.com/13111056001/1004421")# ブラウザでアクセスする
             time.sleep(1)#処理待ち
             html = driver.page_source
             driver.close()#ウェブページを閉じる
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":web access end")
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":web access end")
             driver = None
             soup = BeautifulSoup(html, "html.parser") # BeautifulSoupで扱えるようにパースします
             html = None
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":html parse")
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":html parse")
             txt = soup.text#テキストデータのみ抽出
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":html to str")
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":html to str")
             soup = None
             txt = txt[txt.find("お届け時間")+5:]#以下の２行で時間のみに加工
             txt = txt[:txt.find("分"):]
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":str processing")
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":str processing")
             if txt.isdigit():#整数かどうか判断、整数の場合はループを抜ける                
                 retry_bool = True               
                 break
@@ -65,14 +48,14 @@ while True:
         if retry_bool == True:
             delivery_time = int(txt)
             Take_out_time = delivery_time-10 #テイクアウト時間算出
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":Take_out_time processing")
-            print (datetime.datetime.now().strftime("%H:%M:%S"),"Take-out ",Take_out_time,"delivery ",delivery_time)
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":Take_out_time processing")
+            logging.info  (datetime.datetime.now().strftime("%H:%M:%S"),"Take-out ",Take_out_time,"delivery ",delivery_time)
             Sixteen_segment_deta= SSD.str_to_Sixteen_segment_display(str(Take_out_time))
-            logging.info(datetime.datetime.now().strftime("%H:%M:%S"),":Sixteen_segment_deta processing")
-            print(Sixteen_segment_deta)
+            logging.debug(datetime.datetime.now().strftime("%H:%M:%S"),":Sixteen_segment_deta processing")
+            logging.debug(Sixteen_segment_deta)
         else:
 
-            print (datetime.datetime.now().strftime("%H:%M:%S")+" can't get time!")
+            logging.info (datetime.datetime.now().strftime("%H:%M:%S")+" can't get time!")
 
         retry_nam = 1
         time.sleep(loop_time-62)
