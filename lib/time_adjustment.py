181d41efca88a1aaa38b24349cbe8a4dff0bcb6d
taka-mein <takamasa3140@gmail.com>
2023-05-07 00:12:07 +0900

    0.6

---
+++ lib/time_adjustment.py
@@ -0,0 +1,17 @@
+import datetime 
+import time
+
+def time_adjustment():
+    time_bool = False
+    while not time_bool:
+        time_now = datetime.datetime.now().minute
+        if -1 < time_now < 60:
+            for i in range(0,60,10):
+                time_division_remainder = (time_now+10)%(i+10)
+                if time_division_remainder == 0:
+                    time_bool = True
+                    break
+                else:
+                    time_bool = False
+                    time.sleep(1)
+    return
